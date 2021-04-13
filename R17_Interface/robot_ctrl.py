f53c89cb9233eb3dabab36f4ca91a171a2740069
rynstewart <ryn@Ryn.localdomain>
2021-04-13 13:23:16 -0500

    Queue added for speed reduction

--- R17_Interface/robot_ctrl.py
+++ R17_Interface/robot_ctrl.py
@@ -1,6 +1,7 @@
 import st, serial, os, subprocess,signal, threading, logging, multiprocessing, pdb, sys, socket
 import time
 import datetime
+import queue
 
 # def receive_signal(signum, stack, robot):
 #     print('Received:', signum)
@@ -8,7 +9,7 @@ import datetime
 def current_milli_time():
     return round(time.time() * 1000)
 
-def kill_signal(stdin, robot, lock,sock):
+def kill_signal(stdin, robot, lock,sock,q):
     while True:
         data,address = sock.recvfrom(4096)
         str_data = data.decode()
@@ -29,15 +30,16 @@ def kill_signal(stdin, robot, lock,sock):
             break
         if "speed" in str_data:
             logging.info("Reducing speed...")
-            lock.acquire()
-            try:
-                robot.set_speed(5000) #set speed to half of default speed
-            finally:
-                robot.flush()
-                lock.release()
-                break
+            q.put(1)
+            break
+            # lock.acquire()
+            # try:
+            #     robot.set_speed(5000) #set speed to half of default speed
+            # finally:
+            #     robot.flush()
+            #     lock.release()
 
-def robot_cmd(robot):
+def robot_cmd(robot, q):
     P1 = ['0','3500','0']
     P2 = ['3000','3500','0']
     try:
@@ -45,6 +47,8 @@ def robot_cmd(robot):
         robot.start()
         robot.cartesian()
         for x in range(5):
+            if not q.empty():
+                robot.set_speed(5000) #set speed to half of default speed
             robot.move_to(P1)
             time.sleep(1)
             robot.move_to(P2)
@@ -60,6 +64,8 @@ def main():
     HOST = '127.0.0.1'
     PORT = 65432
 
+    q = queue()
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((HOST, PORT))
     format = "%(asctime)s: %(message)s"
@@ -69,10 +75,10 @@ def main():
 
     robot = st.StArm('/dev/ttyUSB0')
 
-    x = threading.Thread(target=kill_signal, args=(sys.stdin,robot,lock,s,), daemon=True)
+    x = threading.Thread(target=kill_signal, args=(sys.stdin,robot,lock,s,q,), daemon=True)
     x.start()
 
-    robot_cmd(robot)
+    robot_cmd(robot,q)
     s.close()
 
 if __name__ == "__main__":
